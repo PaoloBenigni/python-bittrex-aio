e4322d71728628beb47b24e0f1d28303e6916ff8
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-17 13:42:46 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -3,23 +3,18 @@
 from setuptools import setup
 
 
-setup(name='python-bittrex',
-      version='0.3.0',
-      url="https://github.com/ericsomdahl/python-bittrex",
-      packages=['bittrex'],
-      modules=['bittrex'],
-      install_requires=['requests'],
-      description='Python bindings for bittrex API.',
-      author='Eric Somdahl',
-      author_email='eric@corsairconsulting.com',
+setup(name='python-bittrex-aio',
+      version='0.0.1',
+      url="https://github.com/BlockHub/python-bittrex-aio",
+      packages=['aiobittrex'],
+      modules=['aiobittrex'],
+      install_requires=['aiohttp', 'aiodns', 'async-timeout', 'cchardet'],
+      description='Asynchronous python bindings for bittrex API.',
+      author='Karel L. Kubat',
+      author_email='karel@blockhub.nl',
       classifiers=[
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
           'Operating System :: OS Independent',
-          'License :: OSI Approved :: MIT License',
           'Development Status :: 3 - Alpha',
           'Topic :: Office/Business :: Financial',
       ])
